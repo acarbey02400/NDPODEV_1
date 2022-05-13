d25267a81f75df9a42244b347637e5421f93160b
ibrahim halil acar <acarwoody01@gmail.com>
2022-05-13 20:10:39 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using Business;
+using Newtonsoft.Json;
+using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Blockchain acarCoin = new Blockchain();
+            acarCoin.AddBlock(new Block(DateTime.Now,null, "{sender:İbrahim,receiver:MaHesh,amount:10}"));
+            Console.WriteLine(JsonConvert.SerializeObject(acarCoin, Formatting.Indented));
+            Console.WriteLine($"Is Chain Valid: {acarCoin.IsValid()}");
+        }
+    }
+}
